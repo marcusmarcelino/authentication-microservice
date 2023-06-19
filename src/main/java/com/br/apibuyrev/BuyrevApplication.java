778aa24505be34f2f2c6e37fe3b7013da1db84d3
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-06-19 16:42:47 -0300

    inicialização do escopo do projeto

---
+++ src/main/java/com/br/apibuyrev/BuyrevApplication.java
@@ -0,0 +1,13 @@
+package com.br.apibuyrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyrevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyrevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/apibuyrev/BuyrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.apibuyrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
