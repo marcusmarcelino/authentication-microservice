32e5f8536816fea47c0b73e3eea4592952c86c07
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-07-03 22:06:28 -0300

    adição de configurações para rodagem do filtro e carregamento do provedor de autenticação implementado

--- src/main/java/com/br/apibuyrev/security/SecurityConfig.java
+++ src/main/java/com/br/apibuyrev/security/SecurityConfig.java
@@ -2,18 +2,33 @@ package com.br.apibuyrev.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final AuthenticationProvider authProvider;
+  private final AuthenticationFilter authFilter;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     http
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests((authz) -> authz
-            .anyRequest().authenticated());
+            .requestMatchers("/api/v1/auth/**").permitAll()
+            .anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authProvider)
+        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
