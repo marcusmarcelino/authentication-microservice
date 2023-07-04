c6a0c515bb8a436a45fc752b7637f694b73b8598
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-07-03 22:05:20 -0300

    implementação do Provedor e Gerenciador de Autenticação

--- src/main/java/com/br/apibuyrev/security/ApplicationConfig.java
+++ src/main/java/com/br/apibuyrev/security/ApplicationConfig.java
@@ -2,6 +2,10 @@ package com.br.apibuyrev.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +24,19 @@ public class ApplicationConfig {
   public UserDetailsService userDetailsService() {
     return username -> userRepository.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
   }
 
   @Bean
