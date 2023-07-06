158bff54cb88b4bde3cdb63fa8dff3c0c0270fb3
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-07-05 23:00:52 -0300

    implementação de tratativa global para exceção de argumentos inválidos

--- src/main/java/com/br/apibuyrev/error/RestExceptionHandler.java
+++ src/main/java/com/br/apibuyrev/error/RestExceptionHandler.java
@@ -1,8 +1,33 @@
 package com.br.apibuyrev.error;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+  @Override
+  @Nullable
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    // Map<String, String> errors = new HashMap<>();
+    // ex.getBindingResult().getAllErrors().forEach((error) -> {
+    // String fieldName = ((FieldError) error).getField();
+    // String errorMessage = error.getDefaultMessage();
+    // errors.put(fieldName, errorMessage);
+    // });
+
+    ResourceMethodArgumentNotValid errorResponse = new ResourceMethodArgumentNotValid(ex, status);
+    return new ResponseEntity<>(errorResponse.getErrorResponse(), HttpStatus.BAD_REQUEST);
+  }
 
 }
