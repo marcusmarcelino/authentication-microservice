e9b7e047103a543a5df816a80264f8bf86a158da
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-07-06 10:34:08 -0300

    nova tratativa de exceção para permissão por tipo de acesso do usuário

--- src/main/java/com/br/apibuyrev/modules/user/controller/UserController.java
+++ src/main/java/com/br/apibuyrev/modules/user/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.br.apibuyrev.modules.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
   @Autowired
   private UserService userService;
