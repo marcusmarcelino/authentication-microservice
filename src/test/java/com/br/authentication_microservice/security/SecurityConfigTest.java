f0ca3f2aebd1665b4b11b7fe9ab24c89b4d0a65e
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-08-09 14:10:02 -0300

    corrected test configs

--- src/test/java/com/br/authentication_microservice/security/SecurityConfigTest.java
+++ src/test/java/com/br/authentication_microservice/security/SecurityConfigTest.java
@@ -3,10 +3,13 @@ package com.br.authentication_microservice.security;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SecurityConfigTest {
   @Autowired
